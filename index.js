e1bd0c0a191da7a17aedd4cb39c937456448365a
hamad9iq <ahmed92alrahal@gmail.com>
2026-06-26 04:51:17 +0300

    Remove duplicate Google site verification meta tag

--- index.js
+++ index.js
@@ -59,8 +59,8 @@ function baseLayout(title, description, canonical, ogImage, content, extraHead =
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
-<meta name="google-site-verification" content="7Q0EJk3kQKNLNzIhyzH4k5CsuHsQEa-U0Pwp_w_b0n0" />
 <meta charset="UTF-8">
+<meta name="google-site-verification" content="7Q0EJk3kQKNLNzIhyzH4k5CsuHsQEa-U0Pwp_w_b0n0" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>${title}</title>
 <meta name="description" content="${description}">
